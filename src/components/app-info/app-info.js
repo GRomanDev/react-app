e301524a82dd8224b8de5704db4db0ad7e919c2f
Roman <grromf@gmail.com>
2022-03-21 19:35:55 +0800

    created: markup of app

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,9 +1,9 @@
-import './app-info.css';
+import "./app-info.css";
 
 const AppInfo = () => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании</h1>
+            <h1>Учет сотрудников в компании MedArt</h1>
             <h2>Общее число сотрудников:</h2>
             <h2>Премию получат:</h2>
         </div>
