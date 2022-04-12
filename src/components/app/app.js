f343fdc9508da86f78a058b83c2fa2e3e96ac2ef
Roman <grromf@gmail.com>
2022-04-12 21:42:03 +0800

    added: addItem method in EmployeesAddForm

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,12 +18,28 @@ class App extends Component {
                 {name: 'Алиса Г.', salary: 2000, increase: true, id: 3},
             ]
         }
+        this.maxId = 4;
     }
     
     deleteItem = (id) => {
         this.setState(({data}) => {
             return {
                 data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+                name,
+                salary,
+                increase: false,
+                id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
             }
         })
     }
@@ -41,7 +57,7 @@ class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}/>
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
     )
     }
