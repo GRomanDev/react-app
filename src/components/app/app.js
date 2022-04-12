ebad2b8e513b55b8597c8e31a2aac0867233fc82
Roman <grromf@gmail.com>
2022-04-12 10:34:55 +0800

    added: deletItem method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,27 +8,43 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import "./app.css";
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Петр И.', salary: 700, increase: false, id: 1},
+                {name: 'Юлия H.', salary: 3000, increase: true, id: 2},
+                {name: 'Алиса Г.', salary: 2000, increase: true, id: 3},
+            ]
+        }
+    }
+    
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-    const data = [
-        {name: 'Петр И.', salary: 700, increase: false, id: 1},
-        {name: 'Юлия H.', salary: 3000, increase: true, id: 2},
-        {name: 'Алиса Г.', salary: 2000, increase: true, id: 3},
-    ]
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
 
-    return (
-        <div className="app">
-            <AppInfo />
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
 
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
     )
+    }
 }
 
 export default App;
