f1a70fd9e7200fe6c8ebf5855521b55aec6fec52
Roman <grromf@gmail.com>
2022-04-14 10:29:36 +0800

    refac: SearhPanel (searching employees)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ class App extends Component {
                 {name: 'Петр И.', salary: 700, increase: false, rise: true, id: +nextId()},
                 {name: 'Юлия H.', salary: 3000, increase: true, rise: false, id: +nextId()},
                 {name: 'Алиса Г.', salary: 2000, increase: true, rise: false, id: +nextId()},
-            ]
+            ],
+            term: ''
         }        
         setPrefix("");
     }   
@@ -56,23 +57,39 @@ class App extends Component {
                 return item;
             })
         }))
-    }    
+    }
+    
+    searchEmployees =(items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
 
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmployees(data, term);
         return (
             <div className="app">
                 <AppInfo employees={employees}
-                         increased={increased}/>
+                        increased={increased}/>
 
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <AppFilter />
                 </div>
 
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
